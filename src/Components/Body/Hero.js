61d783797b8e26dfd44d888269596c557a498638
Andres <andres-lh@outlook.com>
2020-12-17 09:20:15 -0500

    Button object created

--- src/Components/Body/Hero.js
+++ src/Components/Body/Hero.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import './Hero.css';
-import Video from '../../Assets/videos/drumVideo.mp4'
+import Video from '../../Assets/videos/drumVideo.mp4';
+import {Button} from './Button';
 
 function Hero() {
     return (
         <div className="hero-container">
             <video src={Video} autoPlay loop muted/>
             <h1>Are you ready to rock?</h1>
+            <div className="hero-btns">
+                <Button className='btns' buttonStyle='btn-outline' buttonSize='btn-large'>
+                    Buy now
+                </Button>
+            </div>
         </div>
     )
 }
