e864e99e6909db0fb8b120deee8966cec3601573
Andrés Lara <andres-lh@outlook.com>
2021-03-31 16:47:43 -0500

    code cleaned

--- server/server.js
+++ server/server.js
@@ -2,23 +2,21 @@ import express from 'express';
 import userRouter from './routes/userRoutes.js';
 import mongoose from 'mongoose';
 import productRouter from './routes/productRoutes.js';
-import path from 'path';
-import { fileURLToPath } from 'url';
+import dotenv from 'dotenv';
+
 
 const app = express();
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
+dotenv.config();
 
-const MongoURL = 'mongodb+srv://admin:civilwar1@cluster0.oqyuz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-
-mongoose.connect(process.env.MONGODB_URL || MongoURL, {
+mongoose.connect(process.env.MONGODB_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true
 })
-    .then(db => console.log('MongoDB database connection established successfully'))
+    .then(() => console.log('MongoDB database connection established successfully'))
     .catch(err => console.log(err) )
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT;
 app.listen(port, () => console.log(`Server at http://localhost:${port}`))
 
 if(process.env.NODE_ENV === "production") {
